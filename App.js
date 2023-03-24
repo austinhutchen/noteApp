00c5966dab6691535c9568987c0cd97f86f3aa2a
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-03-24 08:43:40 -0600

    file organizing

--- App.js
+++ App.js
@@ -2,26 +2,9 @@ import { StatusBar } from "expo-status-bar";
 import { ActivityIndicator, Text, View } from "react-native";
 import { styles } from "./components/styles";
 import { useEffect, useState } from "react";
-
+import getJoke from './components/func'
 export default function App() {
-  const getJoke = async () => {
-    try {
-      const res = await fetch(
-        "https://api.quotable.io/search/authors?query=Einst"
-      );
-      if (!res.ok) {
-        console.log("status: ", res.status);
-        // carful you don't get an infinite loop
-        await getJoke();
-      }
-      const json = await res.json();
-      console.log(json);
-    } catch (error) {
-      console.log("catch error", error);
-      // carful you don't get an infinite loop
-      await getJoke();
-    }
-  };
+ 
 
   return (
     <View style={styles.container}>
