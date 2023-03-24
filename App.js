92d3aa55b252ee2be5e9be7ae800e477ab375b69
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-03-23 21:02:33 -0600

    addded everything to one file

--- App.js
+++ App.js
@@ -1,23 +1,45 @@
-import  {StatusBar} from "expo-status-bar";
-import { Text, View} from "react-native";
-import {styles} from "./components/styles";
-import Fquote from "./components/func";
-
-
-
-
-
+import { StatusBar } from "expo-status-bar";
+import { ActivityIndicator, Text, View } from "react-native";
+import { styles } from "./components/styles";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  let [isLoading, setIsLoading] = useState(true);
+  let [error, setError] = useState();
+  let [response, setResponse] = useState();
+  useEffect(() => {
+    fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
+      .then((response) => response.json())
+      .then(
+        (result) => {
+          setIsLoading(false);
+          setResponse(result);
+        },
+        (error) => {
+          setIsLoading(false);
+          setError(error);
+        }
+      );
+  }, []);
+  const fetch = () => {
+    if (isLoading) {
+      return <ActivityIndicator size="large"></ActivityIndicator>;
+    }
+    if (error) {
+      return <Text>{error}</Text>;
+    } else {
+      return <Text> fetch done</Text>;
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>
-      Hello, user
-      </Text>
-        <Fquote/>
+      <Text>Hello, user</Text>
+      <View>
+        {fetch()}
+        <StatusBar style="auto"></StatusBar>
+      </View>
       <StatusBar style="auto" />
     </View>
-   
-
   );
 }
