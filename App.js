682a142894ace6993977c7f9f442ea7b1ad70d9b
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-03-27 20:52:07 -0600

    upgrades

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { StyleSheet, Linking, Text, View, Button } from "react-native";
 import axios from "axios";
-
+import {styles} from './components/styles'
 class App extends Component {
   constructor(props) {
     super(props);
@@ -81,34 +81,5 @@ class App extends Component {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 30,
-    backgroundColor: "olive",
-    // alignItems: 'center',
-    // justifyContent: 'center',
-  },
-  quotebox: {
-    color: "white",
-    fontSize: 16,
-  },
-  buttons: {
-    alignContent: "center",
-    alignItems: "center",
-    fontSize: 16,
-    textAlignVertical: "center",
-    borderRadius: 2,
-    width: 200,
-    height: 35,
-    marginVertical: 8,
-  },
-  random: {
-    padding: 150,
-  },
-  title: {
-    marginBottom: 150,
-  },
-});
 
 export default App;
--- components/styles.js
+++ components/styles.js
@@ -3,17 +3,29 @@ import { StyleSheet} from 'react-native';
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'olive',
-    alignItems: 'center',
-
-    justifyContent: 'center',
-    display:'flex',
+    padding: 25,
+    backgroundColor: "olive",
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
-  Text:{
-    color: 'white',
+  quotebox: {
+    color: "white",
+    fontSize: 16,
   },
-  ActivityIndicator:{
-  marginBottom:35,
+  buttons: {
+    alignContent: "center",
+    alignItems: "center",
+    fontSize: 16,
+    textAlignVertical: "center",
+    borderRadius: 2,
+    width: 200,
+    height: 35,
+    marginVertical: 8,
   },
-
+  random: {
+    padding: 150,
+  },
+  title: {
+    marginBottom: 150,
+  },
 });
