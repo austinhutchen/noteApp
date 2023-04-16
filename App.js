3243a27ca72f58c2baf1f86da85f637dee568322
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-04-16 10:17:12 -0600

    watchman crawl fix

--- App.js
+++ App.js
@@ -20,6 +20,7 @@ class App extends Component {
       "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
 
     axios.get(url).then((res) => {
+      
       let data = res.data.quotes;
       let quoteNum = Math.floor(Math.random() * data.length);
       let randomQuote = data[quoteNum];
@@ -28,6 +29,9 @@ class App extends Component {
         quote: randomQuote["quote"],
         author: randomQuote["author"],
       });
+    }).catch(function (error) {
+      // handle error
+      console.log(error);
     });
   }
 
