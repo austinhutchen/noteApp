d384adcf6148ad96afa516b5605738ea5a77bdf4
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-05-05 09:06:10 -0600

    skeleton

--- components/styles.js
+++ components/styles.js
@@ -4,7 +4,7 @@ export const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-    backgroundColor: "darkolivegreen",
+    backgroundColor: "darkslategrey",
     // alignItems: 'center',
     // justifyContent: 'center',
   },
