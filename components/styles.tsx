eb1ede9c42662f31aa1919dfcbb56a23e3bf8a14
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-11-16 22:17:16 -0800

    d

--- components/styles.tsx
+++ components/styles.tsx
@@ -4,7 +4,7 @@ export const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-    backgroundColor: "darkslateblue",
+    backgroundColor: "olive",
     // alignItems: 'center',
     // justifyContent: 'center',
   },
