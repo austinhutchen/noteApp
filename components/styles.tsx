4534bcb4fb54852db3b292944c764b24c0571380
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-11-30 08:02:10 -0700

    css formatting

--- components/styles.tsx
+++ components/styles.tsx
@@ -4,37 +4,20 @@ export const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-    backgroundColor: "olive",
+    backgroundColor: "#42b883",
     // alignItems: 'center',
     // justifyContent: 'center',
   },
 
-  buttons: {
-    alignContent: "center",
-    alignItems: "center",
-    fontSize: 16,
-    textAlignVertical: "center",
-    borderRadius: 2,
-    width: 200,
-    height: 35,
-    marginVertical: 8,
-  },
 
-  random: {
-    padding: 150,
-  },
-
-  title: {
-    marginBottom: 20,
-  },
   author: {
     color: "white",
     fontSize: 25,
   },
   quote: {
     alignSelf: "center",
-    color: "yellowgreen",
-    fontSize: 16,
+    color: "white",
+    fontSize: '14em',
     fontStyle: "italic",
     padding: 20,
     borderLeftWidth: 4,
